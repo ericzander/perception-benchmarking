9c299e2ebfbb64a414799d1ce3f810a8729610df
Eric Zander <60997515+ericzander@users.noreply.github.com>
2026-07-07 22:12:41 +0000

    Added frame cap for image listener and validated with more complex environment

--- ros2_ws/src/perception_bridge/perception_bridge/image_listener.py
+++ ros2_ws/src/perception_bridge/perception_bridge/image_listener.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 import rclpy
 from rclpy.node import Node
@@ -5,7 +7,8 @@ from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-OUTPUT_PATH = "/workspace/project/data/latest_rgb.png"
+OUTPUT_PATH = "/workspace/project/ros2_ws/latest_rgb.png"
+SAVE_HZ = 1.0
 
 
 class ImageListener(Node):
@@ -13,11 +16,17 @@ class ImageListener(Node):
         super().__init__("image_listener")
         self.bridge = CvBridge()
         self.count = 0
+        self.last_saved = 0.0
         self.subscription = self.create_subscription(
             Image, "/rgb", self.on_image, qos_profile_sensor_data
         )
 
     def on_image(self, msg: Image):
+        now = time.monotonic()
+        if now - self.last_saved < 1.0 / SAVE_HZ:
+            return
+        self.last_saved = now
+
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         cv2.imwrite(OUTPUT_PATH, frame)
         self.count += 1
